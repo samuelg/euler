8e7f356843cd663e4b54e0e260c6a2813d79b56d
Samuel Galarneau <samuelg@samuelg-laptop.(none)>
2009-11-25 10:54:12 -0600

    added solution to problem 6.

---
+++ problem6.py
@@ -0,0 +1,13 @@
+# dump approach
+import time
+then = time.time()
+
+def sum_list(nums):
+    """ Sums all the numbers in a list. """
+    return reduce(lambda x,y: x + y, nums)
+
+square_sum = sum_list(range(1,101))** 2
+sum_squares = sum_list([x**2 for x in range(1,101)])
+
+print 'difference = %s' % str(square_sum - sum_squares)
+print 'runtime %s' % (time.time() - then)
